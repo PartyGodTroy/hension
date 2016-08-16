21e45c15fb4120806d64bd6f35d46e4736deb9db
Ventroy Rolle <kidveno@live.com>
2016-08-16 00:35:42 -0400

    Cleaned up starter kit, added gulp build

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minify = require('gulp-minify-css');
+
+
+gulp.task('less-styles', function() {
+    gulp.src(['./less'])
+    .pipe(less())
+    .pipe(minify)
+    .pipe(gulp.dest('./css'))
+});
